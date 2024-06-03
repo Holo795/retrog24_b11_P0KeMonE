825f22b0ee86ac42451cf257954aa30db45e3772
Paul Beaujouan <pbeaujou@etudiant.univ-lr.fr>
2024-06-03 11:21:10 +0200

    init commit

--- P0KeMonE/App/player.cpp
+++ P0KeMonE/App/player.cpp
@@ -1,3 +1,32 @@
 #include "player.h"
+#include <QGraphicsItem>
 
-Player::Player() {}
+Player::Player(int x, int y) {}
+
+void Player::move(int dX, int dY)
+{
+
+    QGraphicsRectItem *player = new QGraphicsRectItem(itsX, itsY, 16, 16);
+    if(dX == 1 && dY == 0)
+    {
+        player->setPos(itsX += 1, itsY);
+    }
+    else if (dX == -1 && dY == 0)
+    {
+        player->setPos(itsX += -1, itsY);
+    }
+    else if (dX == 0 && dY == 1)
+    {
+        player->setPos(itsX, itsY += 1);
+    }
+    else if (dX == 0 && dY == -1)
+    {
+        player->setPos(itsX, itsY += -1);
+    }
+
+}
+
+void Player::updateSprite(int dX, int dY)
+{
+
+}
--- P0KeMonE/App/player.h
+++ P0KeMonE/App/player.h
@@ -4,10 +4,50 @@
 #include <QPixmap>
 #include <QGraphicsRectItem>
 
+/**
+ * @class Player
+ * @brief The Player class represents a player character in a graphics scene.
+ *
+ * The Player class is a graphical item that can move within a scene.
+ * It keeps track of its position, movement direction, and level.
+ */
 class Player : public QGraphicsRectItem
 {
+private:
+    float itsLevel;  ///< The current level of the player.
+    int itsDx;       ///< The direction of the player's movement along the x-axis.
+    int itsDy;       ///< The direction of the player's movement along the y-axis.
+    int itsX;        ///< The current x position of the player.
+    int itsY;        ///< The current y position of the player.
+
 public:
-    Player();
+    /**
+     * @brief Constructs a Player object at a given position.
+     *
+     * @param x The initial x position of the player.
+     * @param y The initial y position of the player.
+     */
+    Player(int x, int y);
+
+    /**
+     * @brief Moves the player in a specified direction.
+     *
+     * @param dX The direction to move the player along the x-axis.
+     * @param dY The direction to move the player along the y-axis.
+     */
+    void move(int dX, int dY);
+
+    /**
+     * @brief Updates the player's sprite based on direction.
+     *
+     * This function updates the player's graphical representation
+     * depending on the direction of movement.
+     *
+     * @param dX The direction of the player's movement along the x-axis.
+     * @param dY The direction of the player's movement along the y-axis.
+     */
+    void updateSprite(int dX, int dY);
 };
 
 #endif // PLAYER_H
+

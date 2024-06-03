7acdda3efb48ae743f28f6debbdd8844f3ec6810
lilgar77 <119511417+lilgar77@users.noreply.github.com>
2024-06-03 11:13:53 +0200

    Create class Pokemon
    Creation pokemon Class and take demage method

--- P0KeMonE/App/pokemon.cpp
+++ P0KeMonE/App/pokemon.cpp
@@ -1,3 +1,37 @@
 #include "pokemon.h"
 
-Pokemon::Pokemon() {}
+Pokemon::Pokemon(string itsName, PKTYPE itsType, int itsHealth, int itsSpeed, int itsAtk, int itsSpAtk, int itsDef, int itsSpDef, int itsLevel)
+{
+    this->itsName = itsName;
+    this->itsType = itsType;
+    this->itsHealth = itsHealth;
+    this->itsSpeed = itsSpeed;
+    this->itsAtk = itsAtk;
+    this->itsSpAtk = itsSpAtk;
+    this->itsDef = itsDef;
+    this->itsSpDef = itsSpDef;
+    this->itsLevel = itsLevel;
+}
+
+Pokemon::~Pokemon()
+{
+    
+}
+
+bool Pokemon::isDead(Pokemon &target)
+{
+    return false;
+    
+}
+
+void Pokemon::takeDamage(int damage)
+{
+
+    itsHealth = itsHealth - damage;
+}
+
+void Pokemon::attack(Pokemon &target, Move move)
+{
+
+    
+}

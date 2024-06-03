1e91dd1acde6f50d3a30fd8d96546c51001552b7
ɧσℓσ <paulinsegura79@gmail.com>
2024-06-03 11:06:30 +0200

    Model implementation
    Reading file to load map

--- P0KeMonE/App/model.cpp
+++ P0KeMonE/App/model.cpp
@@ -1,3 +1,24 @@
 #include "model.h"
 
 Model::Model() {}
+
+void Model::loadMap(const QString& filename) {
+    QFile file(filename);
+    if (file.open(QIODevice::ReadOnly)) {
+        QTextStream in(&file);
+        int rows = in.readLine().toInt();
+        int cols = in.readLine().toInt();
+        map.resize(rows, std::vector<char>(cols));
+
+        for (int i = 0; i < rows; ++i) {
+            QString line = in.readLine();
+            for (int j = 0; j < cols; ++j) {
+                map[i][j] = line[j].toLatin1();
+            }
+        }
+    }
+}
+
+const std::vector<std::vector<char>>& Model::getMap() const {
+    return map;
+}
--- P0KeMonE/App/model.h
+++ P0KeMonE/App/model.h
@@ -7,7 +7,25 @@
 class Model
 {
 public:
+    /**
+     * @brief Model constructor
+     */
     Model();
+    /**
+     * @brief loadMap method to load the map from a file
+     * sets the map variable to the map loaded from the file
+     * @param filename
+     */
+    void loadMap(const QString& filename);
+    /**
+     * @brief getMap method to get the map
+     * @return the map
+     */
+    const std::vector<std::vector<char>>& getMap() const;
+
+private:
+    std::vector<std::vector<char>> map; ///< The map of the game
+
 };
 
 #endif // MODEL_H

4245b3b990ba2c76f74f71e46c2706478949c660
ɧσℓσ <paulinsegura79@gmail.com>
2024-06-04 14:05:21 +0200

    Make Doxygen

--- P0KeMonE/App/player.h
+++ P0KeMonE/App/player.h
@@ -1,3 +1,8 @@
+/**
+ * @file player.h
+ * @brief Defines the Player class which handles player actions and collisions.
+ */
+
 #ifndef PLAYER_H
 #define PLAYER_H
 
@@ -6,17 +11,38 @@
 #include <QObject>
 #include <QKeyEvent>
 
+/**
+ * @class Player
+ * @brief The Player class represents a player character in the game.
+ *
+ * The Player class is responsible for handling player input and interactions
+ * within the game scene, including movement and collision detection.
+ */
 class Player : public QObject, public QGraphicsPixmapItem {
     Q_OBJECT
 
 public:
+    /**
+     * @brief Constructs a Player object.
+     * @param parent The parent QGraphicsItem, default is nullptr.
+     */
     Player(QGraphicsItem *parent = nullptr);
 
 private:
-    float scale = 1.8;
+    float scale = 1.8; ///< The scale factor for the player.
+
+    /**
+     * @brief Checks for collisions at the new position.
+     * @param newPos The new position to check for collisions.
+     * @return true if there is a collision, false otherwise.
+     */
     bool checkCollision(QPointF newPos);
 
 protected:
+    /**
+     * @brief Handles key press events for player movement.
+     * @param event The key event.
+     */
     void keyPressEvent(QKeyEvent *event) override;
 };
 

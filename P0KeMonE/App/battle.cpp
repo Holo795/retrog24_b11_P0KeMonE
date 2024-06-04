22c16b82acd9d84674b94d6f050f508d3438a849
ACHILE CORNILLEAU <achile.cornilleau@etudiant.univ-lr.fr>
2024-06-04 16:04:35 +0200

    Finishing the battle class

--- P0KeMonE/App/battle.cpp
+++ P0KeMonE/App/battle.cpp
@@ -2,11 +2,12 @@
 #include <cstdlib>
 #include <ctime>
 
-Battle::Battle(Player *player1, Pokemon *pokemon1, BattleHUD *battleHUM1)
+Battle::Battle(Player *player1, Pokemon *opponent1, BattleHUD *battleHUD1)
 {
-    this->itsPlayer1 = player1;
-    this->itsPokemon1 = pokemon1;
-    this->itsBattleHUM1 = battleHUM1;
+
+    itsPlayer1 = player1;
+    this->itsOpponent1 = opponent1;
+    this->itsBattleHUD1 = battleHUD1;
 }
 
 void Battle::attack(Move *move, Pokemon *target)
@@ -14,22 +15,47 @@ void Battle::attack(Move *move, Pokemon *target)
     // Initialiser le générateur de nombres aléatoires
     srand(time(0));
     int max = 100;
-    int chancesucces = move->getItsAccuracy();
+    int random = (rand() % max) + 1;
+    int successRate = move->getItsAccuracy();
+    int atk, def, lvl, damage;
+    bool crit = random>6;
 
-    if(move->getItsType() == 0)
-   {
-        int random = (rand() % max) + 1;
-        if (random <= chancesucces)
+    Pokemon* pokemon1 = itsPlayer1->getTeam().front();
+    if (target == itsOpponent1)
+    {
+        lvl = pokemon1->getLvl();
+        if(move->getItsType() == 0) //physique
         {
-            target->takeDamage(move->getItsPower());
+            atk = pokemon1->getAtk();
+            def = itsOpponent1->getDef();
+            damage = move->calculateDamage(lvl,atk,def,crit);
+        }
+        else if (move->getItsType() == 1) //special
+        {
+            atk = pokemon1->getSpAtk();
+            def = itsOpponent1->getSpDef();
+            damage = move->calculateDamage(lvl,atk,def,crit);
+        }
+
+    }
+    else
+    {
+        lvl = itsOpponent1->getLvl();
+        if(move->getItsType() == 0) //physique
+        {
+            atk = itsOpponent1->getAtk();
+            def = pokemon1->getDef();
+            damage = move->calculateDamage(lvl,atk,def,crit);
+        }
+        else if (move->getItsType() == 1) //special
+        {
+            atk = itsOpponent1->getSpAtk();
+            def = pokemon1->getSpDef();
+            damage = move->calculateDamage(lvl,atk,def,crit);
         }
     }
-    else if (move->getItsType() == 1)
+    if (random <= successRate)
     {
-        int random = (rand() % max) + 1;
-        if (random <= chancesucces)
-        {
-            target->takeDamage(move->getItsPower());
-        }
+        target->takeDamage(damage);
     }
 }
--- P0KeMonE/App/battle.h
+++ P0KeMonE/App/battle.h
@@ -8,10 +8,10 @@ class Battle
 {
 private:
     Player *itsPlayer1;
-    Pokemon *itsPokemon1;
-    BattleHUD *itsBattleHUM1;
+    Pokemon *itsOpponent1;
+    BattleHUD *itsBattleHUD1;
 public:
-    Battle(Player *player1, Pokemon *pokemon1, BattleHUD *battleHUM1);
+    Battle(Player *itsPlayer1, Pokemon *opponent1, BattleHUD *battleHUD1);
     void attack(Move *move, Pokemon *target);
 
 };

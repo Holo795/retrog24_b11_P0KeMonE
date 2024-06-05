cc99103663948f462f3f23c5296d3b995649b9d6
ɧσℓσ <76647363+Holo795@users.noreply.github.com>
2024-06-06 00:06:25 +0200

    Optimise attack methode and change random system

--- P0KeMonE/App/battle.cpp
+++ P0KeMonE/App/battle.cpp
@@ -1,17 +1,13 @@
 #include "battle.h"
-#include <cstdlib>
-#include <ctime>
 
 /**
  * Constructor for the Battle class.
  * Initializes a new battle instance with given player, opponent, and HUD.
  */
 Battle::Battle(Player *player1, Pokemon *opponent1, BattleHUD *battleHUD1)
-{
-    itsPlayer1 = player1;
-    itsOpponent1 = opponent1;
-    itsBattleHUD1 = battleHUD1;
-}
+    : itsPlayer1(player1), itsOpponent1(opponent1), itsBattleHUD1(battleHUD1),
+    gen(std::random_device{}()) // Seed the generator with random_device
+{}
 
 /**
  * Destructor for the Battle class.
@@ -27,56 +23,26 @@ Battle::~Battle()
  * Conducts an attack in a battle scenario using a specified move against a target Pokémon.
  * The function determines the damage based on move type and calculates whether the attack hits based on move accuracy.
  */
-void Battle::attack(Move *move, Pokemon *target)
-{
-    // Initialize random number generator for attack calculations
-    srand(time(0));
-    int max = 100;
-    int random = rand() % max + 1;
+void Battle::attack(Move *move, Pokemon *target) {
+    std::uniform_int_distribution<> dis(1, 100);  // Distribution for attack calculations
+    int random = dis(gen);
     int successRate = move->getItsAccuracy();
-    int atk, def, lvl, damage;
-    bool crit = random > 6; // Determines if a critical hit occurs
+    bool crit = random > 94; // 6% chance of critical hit
 
-    Pokemon* pokemon1 = itsPlayer1->getTeam().front(); // Retrieves the first Pokémon from the player's team
-    if (target == itsOpponent1) // Determine level and stats based on whether the target is the opponent
-    {
-        lvl = pokemon1->getLvl();
-        if (move->getItsType() == 0) // Physical attack
-        {
-            atk = pokemon1->getAtk();
-            def = itsOpponent1->getDef();
-            damage = move->calculateDamage(lvl, atk, def, crit);
-        }
-        else if (move->getItsType() == 1) // Special attack
-        {
-            atk = pokemon1->getSpAtk();
-            def = itsOpponent1->getSpDef();
-            damage = move->calculateDamage(lvl, atk, def, crit);
-        }
-    }
-    else // Handling when the opponent attacks the player's Pokémon
-    {
-        lvl = itsOpponent1->getLvl();
-        if (move->getItsType() == 0) // Physical attack
-        {
-            atk = itsOpponent1->getAtk();
-            def = pokemon1->getDef();
-            damage = move->calculateDamage(lvl, atk, def, crit);
-        }
-        else if (move->getItsType() == 1) // Special attack
-        {
-            atk = itsOpponent1->getSpAtk();
-            def = pokemon1->getSpDef();
-            damage = move->calculateDamage(lvl, atk, def, crit);
-        }
-    }
-    if (random <= successRate) // Apply damage if attack is successful
-    {
-        itsBattleHUD1->frontDashPokemon(target == itsOpponent1 ? pokemon1 : itsOpponent1);
-        itsBattleHUD1->shakePokemon(target);
-        target->takeDamage(damage);
+    Pokemon* attacker = itsPlayer1->getTeam().front();
+    Pokemon* defender = target == itsOpponent1 ? itsOpponent1 : attacker;
+
+    int lvl = attacker->getLvl();
+    int atk = (move->getItsType() == 0) ? attacker->getAtk() : attacker->getSpAtk();
+    int def = (move->getItsType() == 0) ? defender->getDef() : defender->getSpDef();
+
+    int damage = move->calculateDamage(lvl, atk, def, crit);
+
+    if (random <= successRate) {
+        itsBattleHUD1->frontDashPokemon(attacker);
+        itsBattleHUD1->shakePokemon(defender);
+        defender->takeDamage(damage);
     }
 
-    // Update the battle HUD with the current status of player and opponent Pokémon
     itsBattleHUD1->setPokemon(itsPlayer1->getTeam().front(), itsOpponent1);
 }

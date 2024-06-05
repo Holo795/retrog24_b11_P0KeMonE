c56d78d33f13419969578cb976b8d1883d305146
Lilian Garcia (lilgar77) <119511417+lilgar77@users.noreply.github.com>
2024-06-05 14:58:24 +0200

    base gameover screen

--- P0KeMonE/App/gameoverhud.cpp
+++ P0KeMonE/App/gameoverhud.cpp
@@ -1,3 +1,12 @@
 #include "gameoverhud.h"
+#include "QtWidgets/qgraphicsitem.h"
 
-GameOverHUD::GameOverHUD(QObject *parent) : QGraphicsScene(parent) {}
+GameOverHUD::GameOverHUD(QObject *parent) : QGraphicsScene(parent)
+{
+    // Load and add the background image
+    QImage backgroundImage(":/gameover/assets/gameover.png");
+    QGraphicsPixmapItem* backgroundItem = new QGraphicsPixmapItem(QPixmap::fromImage(backgroundImage));
+    backgroundItem->setPos(0, 0);
+    addItem(backgroundItem);
+
+}

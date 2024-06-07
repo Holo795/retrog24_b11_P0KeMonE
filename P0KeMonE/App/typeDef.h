4583b3ee4066e36e65c84ef8e7221d4e8c041d50
ACHILE CORNILLEAU <achile.cornilleau@etudiant.univ-lr.fr>
2024-06-07 13:30:23 +0200

    Ready to execute

--- P0KeMonE/App/typeDef.h
+++ P0KeMonE/App/typeDef.h
@@ -7,12 +7,24 @@
  * @brief Enumération des types de Pokémon.
  */
 enum PKTYPE {
-    Plante,      /**< Type Plante */
-    Feu,         /**< Type Feu */
-    Eau,         /**< Type Eau */
-    Electrique,  /**< Type Electrique */
-    Sol,       /**< Type Sol */
-    Vol          /**< Type Vol */
+    Grass,      /**< Type Plante */
+    Poison,         /**< Type Feu */
+    Normal,         /**< Type Eau */
+    Bug,        /**< Type Electrique */
+    Psychic,       /**< Type Sol */
+    Ghost,          /**< Type Vol */
+    Ground,          /**< Type Vol */
+    Dragon,          /**< Type Vol */
+    Fairy,          /**< Type Vol */
+    Fire,          /**< Type Vol */
+    Fighting,          /**< Type Vol */
+    Dark,          /**< Type Vol */
+    Electric,          /**< Type Vol */
+    Flying,          /**< Type Vol */
+    Rock,          /**< Type Vol */
+    Steel,          /**< Type Vol */
+    Water,          /**< Type Vol */
+    Ice,          /**< Type Vol */
 };
 
 
